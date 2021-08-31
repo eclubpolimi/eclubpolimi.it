a5aba0fae39da58918680a03aedf6a097e10ae20
Matteo Zanini <matteo.zanini@eclubpolimi.it>
2021-08-31 12:17:47 +0200

    ParagraphTitle è il rettangolo blu con il testo bianco all'interno che separa varie parti della pagina. Prende al suo interno all'istanziamento props.text che setta il testo al paragrafo.

---
+++ src/components/ParagraphTitle/ParagraphTitle.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import ParagraphTitle from './ParagraphTitle';
+import './ParagraphTitle.css';
+import './../../common.css'
+
+export default {
+  component: ParagraphTitle,
+  title: 'Components/ParagraphTitle',
+}
+
+export const Primary = () => <ParagraphTitle/>;
