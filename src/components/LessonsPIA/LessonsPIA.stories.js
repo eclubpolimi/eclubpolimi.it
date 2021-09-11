d9e107842f32cda1fa16c8a39668ca28cb61bc1f
Bob <61847110+DanieleGiorgianni@users.noreply.github.com>
2021-09-11 19:41:03 +0200

    Added Lessons PIA component

---
+++ src/components/LessonsPIA/LessonsPIA.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import LessonsPIA from "./LessonsPIA";
+
+import "./../../common.css";
+
+export default {
+  component: LessonsPIA,
+  title: "Components/LessonsPIA",
+};
+
+export const Primary = () => <LessonsPIA />;
