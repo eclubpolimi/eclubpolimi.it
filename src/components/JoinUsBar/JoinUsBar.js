573bc1a8dcbd1c4e3df983073a64ae4a3443d873
Valeria <Valeriamato00@outlook.com>
2021-09-26 20:03:21 +0200

    Update JoinUsBar Component

--- src/components/JoinUsBar/JoinUsBar.js
+++ src/components/JoinUsBar/JoinUsBar.js
@@ -1,57 +1,26 @@
-import { opacify } from "polished";
 import React, { Component } from "react";
-import { Link } from 'react-router-dom'
-import { setOpacity } from "react"
+import { Link } from "react-router-dom";
 import "./JoinUsBar.css";
-
-/*Attributo "color" per visualizzare la barra rossa (red) o blu (blue)*/
-/*Default: red*/
-
-/*Sistemare opacità*/
-
-const blue = {
-  backgroundColor: "#120068"
-}
-
-const red = {
-  backgroundColor: "#fc3f1a"
-}
-
-const white = {
-  backgroundColor: "#ffffff"
-}
-
-function handleColor_bar(color) {
-  var style_bar = red;
-  if(color == "blue"){
-    style_bar=blue;
-  }
-  return style_bar;
-}
-
-function handleColor_button(color) {
-  var style_button = white;
-  if(color == "blue"){
-    style_button=red;
-  }
-  return style_button;
-}
 
 export default class JoinUsBar extends Component {
   render() {
-    console.log("Il colore della barra JoinUs è:", this.props.color)
     return (
-        <div className="join_us_bar" style ={handleColor_bar(this.props.color)} >
-            <h1 className="JSB_call_to_action">Become one of us!</h1>
-            <button className="JSB_button" 
-                    style ={handleColor_button(this.props.color)}
-                    // onMouseEnter={()=> setOpacity(0.5)}
-                    // onMouseLeave={()=> setOpacity(1)}
-            >
-              <Link to="/JoinUs" ></Link>
-              Join us
-            </button>
-        </div>
+      <div
+        className={` join-us-bar ${
+          this.props.color == "red" ? "bg-orange" : "bg-blue"
+        }`}
+      >
+        <h1 className="JSB-call-to-action">Become one of us!</h1>
+        <Link to="/JoinUs">
+          <div
+            className={`JSB-button ${
+              this.props.color == "red" ? "JSB-white" : "bg-orange"
+            }`}
+          >
+            Join us
+          </div>
+        </Link>
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
-import JoinUsBar from "./components/JoinUsBar";
 import Home from "./views/Home";
 import About from "./views/About";
 import Events from "./views/Events";
@@ -19,7 +18,6 @@ function Site() {
   return (
     <Router basename={pathName}>
       <Navbar />
-      <JoinUsBar  />
       <Switch>
         <Route exact path="/" component={Home}></Route>
         <Route exact path="/About" component={About}></Route>
