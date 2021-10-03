1132f984056cd2e6be7ecc8f878c8108fb6fc575
Bob <61847110+DanieleGiorgianni@users.noreply.github.com>
2021-10-03 15:29:46 +0200

    Added Lessons PIA component (#16)
    * Added Lessons PIA component
    
    * Applied structural changes
    
    * Added index.js and minor CSS changes

---
+++ src/components/LessonsPIA/LessonsPIA.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./LessonsPIA.css";
+
+export default class LessonsPIA extends Component {
+  static propTypes = {
+    //arrLessons: Array of PIA Lessons
+    arrLessons: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)),
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = { activeTabID: this.props.arrLessons[0].id };
+  }
+
+  //to change the state of a button based on the click
+  changeTab = (tabID) => {
+    this.setState({ activeTabID: tabID });
+  };
+
+  render() {
+    const activeTab = this.props.arrLessons[this.state.activeTabID - 1];
+    return (
+      <div>
+        {/* External div */}
+        <div className="lessons-pia-tabs">
+          {/* to set up all the buttons */}
+          <TabButtons
+            activeTabID={this.state.activeTabID}
+            changeTabFunc={this.changeTab}
+            arrLessons={this.props.arrLessons}
+          />
+          {/* to define the lesson content of the selected button */}
+          <div className="lessons-pia-tab-content">
+            <div label={activeTab.name}>
+              <div>
+                <h2>{activeTab.name}</h2>
+                <div className="lessons-pia-description-container">
+                  <div className="lessons-pia-image">
+                    <img
+                      src={activeTab.image}
+                      alt={activeTab.alt}
+                      width="100%"
+                      height="100%"
+                    ></img>
+                  </div>
+                  <div className="lessons-pia-description">
+                    {activeTab.description}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> /* End external div */
+    );
+  }
+}
+
+const TabButtons = ({ changeTabFunc, activeTabID, arrLessons }) => {
+  return (
+    <div className="lessons-pia-tab-buttons">
+      {/* to map all the lessons to their correspondig button */}
+      {arrLessons.map((lesson) => {
+        return (
+          <button
+            className={
+              lesson.id === activeTabID
+                ? "lessons-pia-active"
+                : "lessons-pia-inactive"
+            }
+            onClick={() => changeTabFunc(lesson.id)}
+          >
+            {lesson.name}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
---
+++ src/components/LessonsPIA/LessonsPIA.stories.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+import LessonsPIA from "./LessonsPIA";
+import icon from "../../assets/img-lessons-pia-346x220.jpg";
+
+import "./../../common.css";
+
+export default {
+  component: LessonsPIA,
+  title: "Components/LessonsPIA",
+};
+
+export const Primary = () => (
+  <LessonsPIA
+    arrLessons={[
+      {
+        id: 1,
+        name: "Lezione 1",
+        image: icon,
+        alt: "Passion in Action lesson image",
+        description: "TODO: Inserire descrizione",
+      },
+      {
+        id: 2,
+        name: "Lezione 2",
+        image: icon,
+        alt: "Passion in Action lesson image",
+        description: "TODO: Inserire descrizione",
+      },
+      {
+        id: 3,
+        name: "Lezione 3",
+        image: icon,
+        alt: "Passion in Action lesson image",
+        description: "TODO: Inserire descrizione",
+      },
+      {
+        id: 4,
+        name: "Lezione 4",
+        image: icon,
+        alt: "Passion in Action lesson image",
+        description: "TODO: Inserire descrizione",
+      },
+      {
+        id: 5,
+        name: "Lezione 5",
+        image: icon,
+        alt: "Passion in Action lesson image",
+        description: "TODO: Inserire descrizione",
+      },
+      /* if another lesson needs to be added, copy the format above */
+    ]}
+  />
+);
