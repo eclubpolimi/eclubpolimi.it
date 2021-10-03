fab1b047b493e9221123d342b881561bb6b620d7
Francesco Maccari <f.maccari@infos.it>
2021-10-03 15:25:11 +0200

    Created footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./Footer.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebookSquare, faInstagramSquare, faLinkedin} from "@fortawesome/free-brands-svg-icons";
+
+export default class Hero extends Component {
+  propTypes = {
+    // Logo of the footer
+    logo: PropTypes.string,
+    facebookUrl: PropTypes.string,
+    instagramUrl: PropTypes.string,
+    linkedinUrl: PropTypes.string,
+    copyrightName: PropTypes.string,
+  };
+
+  render() {
+    return (
+      <div class="footer bg-blue">
+        <div class="footer-content">
+          <img class="footer-logo" src={this.props.logo} alt="Logo" />
+          <div class="footer-social">
+              <div className="footer-social-icon footer-facebook">
+                  <a href={this.props.facebookUrl} target="_blank">
+                  <FontAwesomeIcon size="3x" icon={faFacebookSquare}/>
+                  </a>
+              </div>
+              <div className="footer-social-icon footer-instagram">
+                  <a href={this.props.instagramUrl} target="_blank">
+                  <FontAwesomeIcon size="3x" icon={faInstagramSquare}/>
+                  </a>
+              </div>
+              <div className="footer-social-icon footer-linkedin">
+                  <a href={this.props.linkedinUrl} target="_blank">
+                  <FontAwesomeIcon size="3x" icon={faLinkedin}/>
+                  </a>
+              </div>
+          </div>
+        </div>
+        <div class="footer-copyright">
+          Copyright © 2021 - 2021 {this.props.copyrightName}. All rights
+          reserved.
+        </div>
+      </div>
+    );
+  }
+}
