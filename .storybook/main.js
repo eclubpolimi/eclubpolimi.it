d008f78b5584adadbbe9fc4482c850dbd14f1aa9
lynxnb <niccolo.betto@gmail.com>
2021-10-06 21:25:37 +0200

    Add Tailwind CSS
    - Now using `craco` instead of `react-scripts`
    - Adjusted storybook to use craco and tailwind

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,7 @@ module.exports = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
+    "@storybook/addon-postcss",
+    "storybook-preset-craco",
   ],
 };
