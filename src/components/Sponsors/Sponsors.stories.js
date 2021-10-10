7817e52102d8f001a81cb61f03726b998aae6542
Francesco Maccari <f.maccari@infos.it>
2021-10-10 18:57:01 +0200

    Fixed UI bugs on sponsors, added Polimi logo in Footer

--- src/components/Sponsors/Sponsors.stories.js
+++ src/components/Sponsors/Sponsors.stories.js
@@ -11,12 +11,29 @@ export default {
   title: "Components/Sponsors",
 };
 
+<<<<<<< HEAD
 export const Primary = () => (
   <Sponsors
     arrLogos={[
       { src: Image1, href: "#", alt: "Test1" },
       { src: Image2, href: "#", alt: "Test2" },
       { src: Image3, href: "#", alt: "Test3" },
+=======
+export const Primary = () => (<Sponsors 
+  arrLogos={[
+    { src: Image1, href: "#", alt: "Test1" },
+    { src: Image2, href: "#", alt: "Test2" },
+    { src: Image3, href: "#", alt: "Test3" },
+    { src: Image1, href: "#", alt: "Test1" },
+    { src: Image2, href: "#", alt: "Test2" },
+    { src: Image3, href: "#", alt: "Test3" },
+    { src: Image1, href: "#", alt: "Test1" },
+    { src: Image2, href: "#", alt: "Test2" },
+    { src: Image3, href: "#", alt: "Test3" },
+    { src: Image1, href: "#", alt: "Test1" },
+    { src: Image2, href: "#", alt: "Test2" },
+    { src: Image3, href: "#", alt: "Test3" },
+>>>>>>> f48b370 (Fixed UI bugs on sponsors,)
     ]}
   />
 );
