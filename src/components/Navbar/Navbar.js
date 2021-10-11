3b73d663e6ecd37626ec44a032d93ab3d31151aa
lynxnb <niccolo.betto@gmail.com>
2021-10-11 22:59:51 +0200

    Navbar and misc fixes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import logo_white from "../../assets/logo_white.svg";
 
@@ -7,30 +8,11 @@ import { faTimes, faBars } from "@fortawesome/free-solid-svg-icons";
 
 import "./Navbar.css";
 
-const MenuItems = [
-  {
-    title: "About",
-    target: "/About",
-    cName: "nav-link",
-  },
-  {
-    title: "Eventi",
-    target: "/Events",
-    cName: "nav-link",
-  },
-  {
-    title: "Ospiti",
-    target: "/Hosts",
-    cName: "nav-link",
-  },
-  {
-    title: "Passion in action",
-    target: "/PassionInAction",
-    cName: "nav-link",
-  },
-];
-
 export default class Navbar extends Component {
+  static propTypes = {
+    items: PropTypes.array.isRequired,
+  };
+
   state = {
     clicked: false,
   };
@@ -54,20 +36,15 @@ export default class Navbar extends Component {
         <ul
           className={`nav-links bg-blue ${this.state.clicked ? "active" : ""}`}
         >
-          {MenuItems.map((item, index) => {
+          {this.props.items.map((item, index) => {
             return (
-              <li key={index} className={item.cName}>
+              <li key={index} className={item.type}>
                 <Link to={item.target} onClick={this.closeMenu}>
                   {item.title}
                 </Link>
               </li>
             );
           })}
-          <li>
-            <Link to="/Join" className="button-joinus">
-              Join us
-            </Link>
-          </li>
         </ul>
         <div className="menu-icon">
           {this.state.clicked ? (
