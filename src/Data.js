3b83e85bddf02e0f38b0f6465a67a920c7008c21
Francesco Maccari <f.maccari@infos.it>
2021-10-25 10:35:08 +0200

    Added components to About Us page

--- src/Data.js
+++ src/Data.js
@@ -1,6 +1,7 @@
 import Image1 from "assets/test1.png";
 import Image2 from "assets/test2.png";
 import Image3 from "assets/test3.png";
+import Img from "assets/who_we_are_img_1.jpg";
 
 import descriptionLogo from "assets/logo-512x512.png";
 import homepageHeroBackground from "assets/homepage_hero.jpg";
@@ -91,6 +92,65 @@ export default class SiteData {
   };
 
   static HomepageHeroBackground = homepageHeroBackground;
+
+  // ABOUT US
+  static AboutDescription = {
+    title: "Our mission",
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  };
+
+  static Teams = [
+    {
+      teamName: "Team test 1",
+      teamComponents: [
+        {
+          name: "Nome cognome",
+          image: Img,
+          description: "Lorem ipsum dolor sit amet...",
+        },
+        {
+          name: "Nome cognome",
+          image: Img,
+          description: "Lorem ipsum dolor sit amet...",
+        },
+        {
+          name: "Nome cognome",
+          image: Img,
+          description: "Lorem ipsum dolor sit amet...",
+        },
+        {
+          name: "Nome cognome",
+          image: Img,
+          description: "Lorem ipsum dolor sit amet...",
+        },
+        {
+          name: "Nome cognome",
+          image: Img,
+          description: "Lorem ipsum dolor sit amet...",
+        },
+      ],
+    },
+    {
+      teamName: "Team test 1",
+      teamComponents: [
+        {
+          name: "Nome cognome",
+          image: Img,
+          description: "Lorem ipsum dolor sit amet...",
+        },
+      ],
+    },
+    {
+      teamName: "Team test 1",
+      teamComponents: [
+        {
+          name: "Nome cognome",
+          image: Img,
+          description: "Lorem ipsum dolor sit amet...",
+        },
+      ],
+    },
+  ];
 
   // LESSONS PIA (placeholder)
 
--- src/views/About/About.js
+++ src/views/About/About.js
@@ -1,8 +1,35 @@
 import React, { Component } from "react";
 import "./About.css";
 
+import SiteData from "Data";
+
+import Hero from "components/Hero";
+import Description from "components/Description";
+import Teams from "components/Teams";
+import JoinUsBar from "components/JoinUsBar";
+
 export default class About extends Component {
   render() {
-    return <div className="about">About</div>;
+    return (
+      <div className="about">
+        <Hero
+          backgroundImage={SiteData.HomepageHeroBackground}
+          height="500px"
+          darkness={0.5}
+          contentType="img"
+          logo={SiteData.LogoWhite}
+        />
+        <div className="md:px-10">
+          <Description
+            title={SiteData.AboutDescription.title}
+            text={SiteData.AboutDescription.text}
+          />
+        </div>
+        <JoinUsBar />
+        <div className="md:px-10">
+          <Teams teams={SiteData.Teams} />
+        </div>
+      </div>
+    );
   }
 }
