5c2ca8e659d624234afbb8695813c02ec78220a4
ValeMTo <Valeriamato00@outlook.com>
2021-11-17 23:24:03 +0100

    Created Carousel-component

---
+++ src/components/Carousel/SliderData.js
@@ -0,0 +1,11 @@
+export const SliderData = [
+  {
+    image: "./../../../public/images/books.jpeg",
+  },
+  {
+    image: "./../../../public/images/ideas.jpeg",
+  },
+  {
+    image: "./../../../public/images/quotes.jpeg",
+  },
+];
