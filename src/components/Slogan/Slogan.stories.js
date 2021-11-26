c94ebff11462f613f4f29077c34297406bce9c19
mvkv <franci.macca@gmail.com>
2021-11-26 15:57:24 +0100

    Created slogan component for "Network" page

---
+++ src/components/Slogan/Slogan.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Slogan from "./Slogan";
+import SiteData from "Data";
+
+import "common.css";
+
+export default {
+  component: Slogan,
+  title: "Components/Slogan",
+};
+
+export const Primary = () => <Slogan slogan={SiteData.networkSlogan} />;
