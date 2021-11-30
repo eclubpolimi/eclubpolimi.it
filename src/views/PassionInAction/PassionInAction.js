7d893998778edafd8e22f2b4a151472ea3e5a1ea
Francesco Maccari <f.maccari@infos.it>
2021-11-30 18:51:24 +0100

    Changed lessonsPIA components to general ones

--- src/views/PassionInAction/PassionInAction.js
+++ src/views/PassionInAction/PassionInAction.js
@@ -1,25 +1,36 @@
 import React, { Component } from "react";
 
-import HeroPIA from "components/HeroPIA";
-import DescriptionPIA from "components/DescriptionPIA";
 import LessonsPIA from "components/LessonsPIA";
 
 import "./PassionInAction.css";
 
 import SiteData from "Data";
+import Hero from "components/Hero";
+import Description from "components/Description";
 
 export default class PassionInAction extends Component {
   render() {
     return (
       <div className="passion-in-action">
-        <HeroPIA heroPIA={SiteData.HeroPIA} />
-        <div className="md:px-10">
-          <DescriptionPIA descriptionPIA={SiteData.DescriptionPIA} />
-        </div>
-        <LessonsPIA
-          lessons={SiteData.LessonsPIA}
-          className="w-3/4 mx-auto mb-16"
+        <Hero
+          backgroundImage={SiteData.PIAHeroBackground}
+          height="500px"
+          darkness={0.5}
+          contentType="text"
+          text="Passion in action"
         />
+        <div className="max-w-screen-lg lg:mx-auto px-5 lg:px-0">
+          <Description
+            title={SiteData.DescriptionPIA.title}
+            text={SiteData.DescriptionPIA.text}
+          />
+        </div>
+        <div className="max-w-screen-lg lg:mx-auto px-5 lg:px-0">
+          <LessonsPIA
+            lessons={SiteData.LessonsPIA}
+            className="w-3/4 mx-auto mb-16"
+          />
+        </div>
       </div>
     );
   }
