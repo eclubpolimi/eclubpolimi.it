3a0254b6b4053a69d5286df13e775090284dda3e
lynxnb <niccolo.betto@gmail.com>
2021-12-01 17:57:40 +0100

    Fix navbar hamburger going out of bounds when active

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -58,21 +58,25 @@ export default class Navbar extends Component {
             );
           })}
         </ul>
-        <svg
-          className={`ham hamRotate ${this.state.clicked ? "nav-active" : ""}`}
-          viewBox="0 0 100 100"
-          onClick={this.handleMenu}
-        >
-          <path
-            className="line top"
-            d="m 70,33 h -40 c 0,0 -8.5,-0.149796 -8.5,8.5 0,8.649796 8.5,8.5 8.5,8.5 h 20 v -20"
-          />
-          <path className="line middle" d="m 70,50 h -40" />
-          <path
-            className="line bottom"
-            d="m 30,67 h 40 c 0,0 8.5,0.149796 8.5,-8.5 0,-8.649796 -8.5,-8.5 -8.5,-8.5 h -20 v 20"
-          />
-        </svg>
+        <div className="ham-container overflow-hidden">
+          <svg
+            className={`ham hamRotate ${
+              this.state.clicked ? "nav-active" : ""
+            }`}
+            viewBox="0 0 100 100"
+            onClick={this.handleMenu}
+          >
+            <path
+              className="line top"
+              d="m 70,33 h -40 c 0,0 -8.5,-0.149796 -8.5,8.5 0,8.649796 8.5,8.5 8.5,8.5 h 20 v -20"
+            />
+            <path className="line middle" d="m 70,50 h -40" />
+            <path
+              className="line bottom"
+              d="m 30,67 h 40 c 0,0 8.5,0.149796 8.5,-8.5 0,-8.649796 -8.5,-8.5 -8.5,-8.5 h -20 v 20"
+            />
+          </svg>
+        </div>
       </nav>
     );
   }
