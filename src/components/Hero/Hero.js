755d63926a892c442f7c514330692f887d08b22b
Francesco Maccari <f.maccari@infos.it>
2021-12-03 04:56:49 +0100

    Added video in hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,8 +5,12 @@ import "./Hero.css";
 export default class Hero extends Component {
   // Props description
   static propTypes = {
+    //backgroundType
+    backgroundType: PropTypes.string,
     // backgroundImage: imported background image
     backgroundImage: PropTypes.string,
+    // backgroundVideo: video link
+    backgroundVideo: PropTypes.string,
     // height: height of hero component
     height: PropTypes.string,
     // darkness: darkness of the background (expressed in percentage from 0 to 1)
@@ -24,21 +28,46 @@ export default class Hero extends Component {
   render() {
     return (
       <div
-        className="hero"
-        style={{
-          backgroundImage: `linear-gradient(rgba(0,0,0,${this.props.darkness}), rgba(0,0,0,${this.props.darkness})), url(${this.props.backgroundImage})`,
-          height: this.props.height,
-          backgroundPosition: "center",
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-        }}
+        className={`hero relative overflow-hidden`}
+        style={
+          this.props.backgroundType === "video"
+            ? { height: this.props.height }
+            : {
+                backgroundImage: `linear-gradient(rgba(0,0,0,${this.props.darkness}), rgba(0,0,0,${this.props.darkness})), url(${this.props.backgroundImage})`,
+                height: this.props.height,
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+              }
+        }
       >
+        {this.props.backgroundType === "video" && (
+          <div>
+            <div
+              className={`absolute z-10 top-0 left-0 right-0 bottom-0 bg-black opacity-${
+                this.props.darkness * 100
+              }`}
+            ></div>
+            <div className="absolute top-0 left-0 right-0 bottom-0">
+              <video
+                preload="auto"
+                className="w-full filter relative -top-1/3"
+                autoPlay="autoplay"
+                loop="loop"
+                muted="muted"
+                playsInline
+              >
+                <source src={this.props.backgroundVideo}></source>
+              </video>
+            </div>
+          </div>
+        )}
         {this.props.contentType === "text" ? (
-          <div className="hero-text">{this.props.text}</div>
+          <div className="z-20 hero-text">{this.props.text}</div>
         ) : (
           <img
             style={{ width: this.props.logoWidth, aspectRatio: 1 }}
-            className="hero-logo"
+            className="z-20 hero-logo"
             src={this.props.logo}
             alt=""
           />
