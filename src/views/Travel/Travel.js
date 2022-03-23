525cfb04b737a950c51fdfe006e56c8ec0b85af6
Cavalli98 <dario1.cavalli@mail.polimi.it>
2022-03-23 17:33:35 +0100

    prettier format

--- src/views/Travel/Travel.js
+++ src/views/Travel/Travel.js
@@ -13,39 +13,52 @@ export default class Travel extends Component {
     return (
       <div className="travel">
         <div className="px-5 lg:px-0 grid grid-cols-5 margin-0 min-h-screen">
-          <div className="h-full w-full col-span-4 grid grid-cols-5 bg-cover"
-              style={{backgroundImage: "url(" + SiteData.travelMainBackgroundOpacity30 + ")"}}>
+          <div
+            className="h-full w-full col-span-4 grid grid-cols-5 bg-cover"
+            style={{
+              backgroundImage:
+                "url(" + SiteData.travelMainBackgroundOpacity30 + ")",
+            }}
+          >
             <div className="col-start-2 col-span-2">
               <Description title="Travel">
                 <p>
-                  <b>Lean Startup: the art to be entrepreneurial</b> is the first
-                  Politecnico course organized in collaboration with a student
-                  association, Entrepreneurship Club Polimi. The course is organized
-                  by students, for students.
+                  <b>Lean Startup: the art to be entrepreneurial</b> is the
+                  first Politecnico course organized in collaboration with a
+                  student association, Entrepreneurship Club Polimi. The course
+                  is organized by students, for students.
                   <br />
                   <br />
-                  The objective of the course is to introduce the students to the
-                  so-called "entrepreneurial mentality": a way of reasoning that
-                  typically belongs to the startup world, but that can be employed
-                  whatever career they will pursue.
+                  The objective of the course is to introduce the students to
+                  the so-called "entrepreneurial mentality": a way of reasoning
+                  that typically belongs to the startup world, but that can be
+                  employed whatever career they will pursue.
                 </p>
               </Description>
             </div>
           </div>
-          <div className="h-128 w-128 bg-cover absolute self-center place-self-end hidden lg:block"
-              style={{backgroundImage: "url(" + SiteData.TravelMainBackground + ")"}}>
-          </div>
+          <div
+            className="h-128 w-128 bg-cover absolute self-center place-self-end hidden lg:block"
+            style={{
+              backgroundImage: "url(" + SiteData.TravelMainBackground + ")",
+            }}
+          ></div>
         </div>
-        
-        <div className="px-5 lg:px-0 grid grid-cols-5 margin-0 min-h-screen bg-cover"
-          style={{backgroundImage: "url(" + SiteData.travelMainBackgroundOpacity30 + ")"}}>
+
+        <div
+          className="px-5 lg:px-0 grid grid-cols-5 margin-0 min-h-screen bg-cover"
+          style={{
+            backgroundImage:
+              "url(" + SiteData.travelMainBackgroundOpacity30 + ")",
+          }}
+        >
           <div className="col-start-2 col-span-3">
             <Description title="Travel">
               <p>
                 <b>Lean Startup: the art to be entrepreneurial</b> is the first
                 Politecnico course organized in collaboration with a student
-                association, Entrepreneurship Club Polimi. The course is organized
-                by students, for students.
+                association, Entrepreneurship Club Polimi. The course is
+                organized by students, for students.
                 <br />
                 <br />
                 The objective of the course is to introduce the students to the
