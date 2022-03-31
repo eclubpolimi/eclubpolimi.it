6571adf75662f46f862da2ee543155e62f918d23
lynxnb <niccolo.betto@gmail.com>
2022-03-31 13:15:02 +0200

    OutlineLabel component
    A rounded rectangle shaped label with an offset outline.

---
+++ src/components/OutlineLabel/OutlineLabel.js
@@ -0,0 +1,19 @@
+const OutlineLabel = ({ invertedColors, text, className = "" }) => {
+  return (
+    <div
+      className={`${className} relative py-4 px-12 w-max rounded-xl
+      ${
+        invertedColors
+          ? "bg-orange after:border-blue"
+          : "bg-blue after:border-orange"
+      }
+      after:block after:absolute after:border-[1.5px] after:border-solid
+      after:top-0 after:left-0 after:right-0 after:bottom-0 after:transform after:-translate-x-0.5 after:-translate-y-1
+      after:content-[''] after:rounded-xl after:pointer-events-none`}
+    >
+      <p className="text-white font-semibold text-2xl leading-tight">{text}</p>
+    </div>
+  );
+};
+
+export default OutlineLabel;
