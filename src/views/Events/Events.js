88a0e8cda485d144eff0234143e984239f046242
lynxnb <niccolo.betto@gmail.com>
2022-04-12 20:55:08 +0200

    Rework `Timeline` component
    * Switch to functional component
    * Switch to CSS modules

--- src/views/Events/Events.js
+++ src/views/Events/Events.js
@@ -14,7 +14,7 @@ export default class Events extends Component {
           autoplay={5000}
         />
         <div className="lg:my-20 my-12 max-w-screen-lg lg:mx-auto px-5 lg:px-0">
-          <Timeline data={SiteData.eventsList} {...{ style: "centered" }} />
+          <Timeline data={SiteData.eventsList} theme="centered" />
         </div>
       </div>
     );
