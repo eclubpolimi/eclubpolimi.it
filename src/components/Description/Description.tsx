1644a0a0c060bb64cac64d54c75c0c5499daeca1
Francesco Maccari <f.maccari@infos.it>
2022-04-16 14:14:54 +0200

    refactor description in typescript

---
+++ src/components/Description/Description.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+type DescriptionProps = {
+  title: string,
+  children: React.ReactNode;
+}
+
+const Description = ({title, children}: DescriptionProps) => {
+
+
+    return (
+      <div className="py-16">
+        <h1 className="md:text-center mb-5">{title}</h1>
+        {children}
+      </div>
+    );
+  
+}
+
+export default Description;
