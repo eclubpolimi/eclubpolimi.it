b287d6d03db418371dc9cd95716b82040d309fbc
ValeMTo <75945633+ValeMTo@users.noreply.github.com>
2022-04-16 13:35:42 +0200

    Startup Challenge Page (#71)
    * Description and pdf Download (not working)
    
    * remove download button
    
    * Remove never-used line
    
    * transform button into functional component,
    add forceAnchor to force <a> tag
    
    * moved pdf into proper folder,
    add button to detailed info pdf,
    fix text alignment
    
    * remove unutilized dependencies
    
    Co-authored-by: Cavalli98 <dario1.cavalli@mail.polimi.it>
    Co-authored-by: Francesco Maccari <f.maccari@infos.it>

--- src/views/StartupChallenge/StartupChallenge.js
+++ src/views/StartupChallenge/StartupChallenge.js
@@ -8,6 +8,24 @@ import LogoStartupChallenge from "assets/logo_startupchallenge.jpg";
 import SiteData from "Data";
 import JoinUsBar from "components/JoinUsBar";
 import ParagraphTitle from "components/ParagraphTitle";
+import Description from "components/Description";
+import StartupChallengePDF from "assets/startupChallenge/Start_Up_Challenge_Document.pdf";
+
+const ProgramButton = () => {
+  return (
+    <div className="w-full flex items-center justify-center pb-10">
+      <Button
+        forceAnchor
+        to={StartupChallengePDF}
+        theme="orange"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Detailed program
+      </Button>
+    </div>
+  );
+};
 
 const StartupChallenge = () => {
   return (
@@ -50,6 +68,23 @@ const StartupChallenge = () => {
           logos={SiteData.StartupChallengeOrgLogos}
         />
       </div>
+      <div className="max-w-screen-lg lg:mx-auto px-5 lg:px-0">
+        <Description title="A two-weekend entrepreneurship event and competition">
+          <p className="text-justify">
+            The University Startup challenge is open to anyone who might be
+            interested, but at least half of the members of each team must be
+            from Politecnico di Milano or Bocconi University. It will be held in
+            person on both campuses, and will include seminars, workshops and
+            networking moments. Participants will be given a challenge by the
+            partner company, and will work together in groups of 4-6 people to
+            create an innovative startup idea that will help solve a practical
+            problem for the city of Milan. The finalists will present their
+            startup pitch in front of a jury of experts and the best teams will
+            be rewarded!
+          </p>
+        </Description>
+      </div>
+      <ProgramButton />
       <ParagraphTitle text="Events schedule" />
       <div className="py-16 max-w-screen-lg lg:mx-auto px-5 lg:px-0">
         <Timeline data={SiteData.StartupChallengeTimeline} theme="split" />
