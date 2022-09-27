cd82be6ba5ad76bb19e750bf6e22a7d7645226aa
lynxnb <niccolo.betto@gmail.com>
2022-09-27 02:16:20 +0200

    Migrate Description component to functional style

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -1,17 +1,12 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React from "react";
 
-export default class Description extends Component {
-  static propTypes = {
-    // title of this paragraph
-    title: PropTypes.string,
-  };
-  render() {
-    return (
-      <div className="py-16">
-        <h2 className="md:text-center mb-5">{this.props.title}</h2>
-        {this.props.children}
-      </div>
-    );
-  }
-}
+const Description = ({ title, children, className = "" }) => {
+  return (
+    <div className={`py-16 ${className}`}>
+      <h2 className="md:text-center mb-5">{title}</h2>
+      {children}
+    </div>
+  );
+};
+
+export default Description;
