74eb0804e20950d7b96acb5ae42929476eaaf7e7
lynxnb <niccolo.betto@gmail.com>
2022-09-27 02:18:01 +0200

    Update Timeline to accept JSX objects as prop

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -21,7 +21,8 @@ const Timeline = ({ theme, data, className }) => {
               <div className={styles.timeline_marker}></div>
               <div className={styles.timeline_content}>
                 <h3 className={styles.timeline_title}>{item.title}</h3>
-                <p>{item.body}</p>
+                {item.body && <p className="mt-2">{item.body}</p>}
+                {item.children && <div className="mt-2">{item.children}</div>}
               </div>
             </li>
           );
