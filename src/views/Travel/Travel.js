ded7352280e058375f6e522018bc55d986caf703
MartinBronzo <martin.bronzo@mail.polimi.it>
2022-09-28 11:47:04 +0200

    add travel script

--- src/views/Travel/Travel.js
+++ src/views/Travel/Travel.js
@@ -11,9 +11,10 @@ const timelineData = [
     title: "Day 1",
     children: (
       <ul className="list-disc ml-4 mt-2">
-        <li>Arrival in Bali</li>
-        <li>Check-in at the hotel</li>
-        <li>Free time</li>
+        <li>Thursday morning (about 06:30): meeting at Milan Linate Airport</li>
+        <li>9:20 departure from Milan Linate Airport with ITA flight</li>
+        <li>11:20 landing at Amsterdam Schiphol</li>
+        <li>Afternoon: visit to the first startup</li>
       </ul>
     ),
   },
@@ -22,9 +23,9 @@ const timelineData = [
     title: "Day 2",
     children: (
       <ul className="list-disc ml-4 mt-2">
-        <li>Arrival in Bali</li>
-        <li>Check-in at the hotel</li>
-        <li>Free time</li>
+        <li>Friday morning: visit to the historical port and neighborhood & lunch at the biggest food market of the Netherlands</li>
+        <li>Friday afternoon: visit to our second startup</li>
+        <li>Bitterballen and drinks together in Delfshaven!</li>
       </ul>
     ),
   },
@@ -33,9 +34,9 @@ const timelineData = [
     title: "Day 3",
     children: (
       <ul className="list-disc ml-4 mt-2">
-        <li>Arrival in Bali</li>
-        <li>Check-in at the hotel</li>
-        <li>Free time</li>
+        <li>Saturday morning: train to Delft and visit to the typical Saturday market</li>
+        <li>Saturday afternoon: free afternoon in Delft</li>
+        <li>Saturday night: Pubquiz</li>
       </ul>
     ),
   },
@@ -44,9 +45,10 @@ const timelineData = [
     title: "Day 4",
     children: (
       <ul className="list-disc ml-4 mt-2">
-        <li>Arrival in Bali</li>
-        <li>Check-in at the hotel</li>
-        <li>Free time</li>
+        <li>free morning</li>
+        <li>h 17 circa: train to Amsterdam Schiphol</li>
+        <li>h 19.50: flight to Milano Linate</li>
+        <li>h 21.25: landing at Milano Linate airport</li>
       </ul>
     ),
   },
@@ -69,14 +71,14 @@ const Travel = () => {
               E-Club goes to the Netherlands
             </h1>
             <span className="mt-3 text-slate-700 font-semibold text-xl block w-fit">
-              Subtitle placeholder
+              Why should I go to the Netherlands?
             </span>
             <TravelBar
               className="mt-8 md:mt-12 w-full lg:w-2/3"
               info={{
                 place: "Rotterdam",
                 date: "17/11/2022",
-                peoples: "55",
+                peoples: "78",
                 price: "€205,00",
                 priceTerms: "after PoliMi discount",
               }}
@@ -88,23 +90,13 @@ const Travel = () => {
       <div className="bg-slate-100">
         <Description className="w-4/5 mx-auto" title="Trip to the Netherlands">
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-            tincidunt, nisl eget aliquam lacinia, nunc nisl aliquet nisl, eget
-            aliquam nisl nisl sit amet nisl. Sed tincidunt, nisl eget aliquam
-            lacinia, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet
-            nisl. Sed tincidunt, nisl eget aliquam lacinia, nunc nisl aliquet
+              You will have the opportunity to dive into one of the most dynamic startup ecosystem in Europe, visiting the best startups in Europe. You’ll get to see:- early-seed startups;- the spectacular modern architecture of Rotterdam;- the student life Delft is famous for;Besides the visits to the Dutch startup world, you’ll have the opportunity to spend a nice and relaxed weekend enjoying a north-European experience surrounded by bikes, canals and the uniquely international Dutch culture!
             <br />
             <br />
-            nisl, eget aliquam nisl nisl sit amet nisl. Sed tincidunt, nisl eget
-            aliquam lacinia, nunc nisl aliquet nisl, eget aliquam nisl nisl sit
-            amet nisl. Sed tincidunt, nisl eget aliquam lacinia, nunc nisl
-            aliquet nisl, eget aliquam nisl nisl sit amet nisl. Sed
+              Join our trip to Rotterdam & Delft (November 17th-20th) to explore these two cities and discover the Dutch startup ecosystem through exclusive company visits!
             <br />
             <br />
-            tincidunt, nisl eget aliquam lacinia, nunc nisl aliquet nisl, eget
-            aliquam nisl nisl sit amet nisl. Sed tincidunt, nisl eget aliquam
-            lacinia, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet
-            nisl. Sed tincidunt, nisl eget aliquam lacinia, nunc nisl aliquet
+              Registration opens on October 6th at 12. 30 tickets available, reserve your seat quickly!
           </p>
         </Description>
       </div>
