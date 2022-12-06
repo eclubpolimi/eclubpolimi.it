c70ebc7733bdd12085fc65ac0150aacbf935de4c
lynxnb <niccolo.betto@gmail.com>
2022-12-06 15:39:44 +0100

    Introduce Notion requests utils
    * Added an utility module for Notion requests helper functions
    * Added Notion client package for performing requests to the Notion APIs
    
    Co-Authored-By: Francesco Maccari <franci.macca@gmail.com>

---
+++ src/utils/notion_requests.ts
@@ -0,0 +1,24 @@
+import { APIResponseError, Client } from "@notionhq/client";
+import { QueryDatabaseResponse } from "@notionhq/client/build/src/api-endpoints";
+
+export const Sources = {
+};
+
+const notion = new Client({ auth: process.env.NOTION_AUTH_TOKEN });
+
+export const getNotionData = async (
+  source: typeof Sources[keyof typeof Sources]
+): Promise<Array<unknown>> => {
+  // Database queries returns either a QueryDatabaseResponse or APIResponseError
+  // This is undocumented in the notion client docs
+  const response: QueryDatabaseResponse | APIResponseError =
+    await notion.databases.query({
+      database_id: source as string,
+    });
+
+  if ("code" in response) {
+    throw Error(`Error: ${(response as APIResponseError).code}`);
+  }
+
+  return response.results as Array<unknown>;
+};
