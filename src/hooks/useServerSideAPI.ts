1aa6382a218867f55d663b5700267d19c0942b4b
Francesco Maccari <franci.macca@gmail.com>
2022-12-06 15:41:52 +0100

    Creates server-side api hook for automatic error handling
    * Creates hook for server side request handling

---
+++ src/hooks/useServerSideAPI.ts
@@ -0,0 +1,29 @@
+type Props<T> = {
+  props: T;
+};
+
+type Redirect = {
+  redirect: {
+    destination: string;
+    permanent: boolean;
+  };
+};
+
+type ServerSideHookResponse<T> = Props<T> | Redirect;
+
+const useServerSideAPI = async <T>(
+  callback: () => Promise<T>
+): Promise<ServerSideHookResponse<T>> => {
+  return await callback()
+    .then((data: T) => ({
+      props: data,
+    }))
+    .catch(() => ({
+      redirect: {
+        destination: "/error",
+        permanent: false,
+      },
+    }));
+};
+
+export default useServerSideAPI;
