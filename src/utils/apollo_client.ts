0a354c91f6feecefcb1c134cdddb251a0b736ddf
lynxnb <niccolo.betto@gmail.com>
2023-03-01 00:14:21 +0100

    Introduce Apollo client as a GraphQL client

---
+++ src/utils/apollo_client.ts
@@ -0,0 +1,16 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: process.env.CMS_GRAPHQL_ENDPOINT,
+  headers: {
+    Authorization: `Bearer ${process.env.CMS_ACCESS_TOKEN}`,
+  },
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: "cache-and-network",
+    },
+  },
+});
+
+export default client;
