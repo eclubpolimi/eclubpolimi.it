2d2abafe039af301d464a57f1353e68a3568a272
AFKabl3 <thisismyworkhotmail@gmail.com>
2025-08-15 01:44:12 +0200

    cleanup

--- src/pages/_disabled/passion-in-action_index.tsx
+++
@@ -1,2 +0,0 @@
-// DISABLED: Page moved to _disabled folder - this was an empty file
-// Content was removed from navbar but page structure was empty
--- src/pages/_disabled/test_index.tsx
+++
@@ -1,2 +0,0 @@
-// DISABLED: Page moved to _disabled folder - this was an empty test file
-// This was created as a test page but is not needed for production
