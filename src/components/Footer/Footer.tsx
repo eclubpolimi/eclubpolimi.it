400e1bfa4789e8417d6ac5a5ae98c289f0f24781
AFKabl3 <thisismyworkhotmail@gmail.com>
2025-08-14 18:25:42 +0200

    feat: Add email contact option  and made buttons reactive

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,6 +4,7 @@ import {
   faInstagramSquare,
   faLinkedin,
 } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import Image from 'next/image';
 import Sponsors from 'components/Sponsors/Sponsors';
 import {
@@ -19,6 +20,7 @@ type FooterProps = {
   instagramUrl: string;
   linkedinUrl: string;
   copyrightName: string;
+  showSponsors?: boolean;
 };
 
 const Footer = ({
@@ -27,6 +29,7 @@ const Footer = ({
   instagramUrl,
   linkedinUrl,
   copyrightName,
+  showSponsors = false,
 }: FooterProps) => {
   const sponsors = useSponsors();
   const logoWhite = useImageAsset('branding_logo_white');
@@ -34,12 +37,14 @@ const Footer = ({
   return (
     <div>
       {/* Sponsors Section */}
-      <div className="bg-ec_background dark:bg-ec_background_darkmode">
-        <Sponsors
-          className="max-w-screen-lg lg:mx-auto px-5 lg:px-0 py-16"
-          logos={sponsors}
-        />
-      </div>
+      {showSponsors && (
+        <div className="bg-ec_background dark:bg-ec_background_darkmode">
+          <Sponsors
+            className="max-w-screen-lg lg:mx-auto px-5 lg:px-0 py-16"
+            logos={sponsors}
+          />
+        </div>
+      )}
 
       {/* Footer Main Section */}
       <div className="flex flex-col items-center p-2 pb-10 bg-ec_blue dark:bg-ec_blue_darkmode">
@@ -67,26 +72,13 @@ const Footer = ({
           {/* Social Media Links */}
           <div className="flex flex-col items-center w-full lg:w-1/2 mt-6 lg:mt-0">
             <h2 className="text-ec_text_darkmode text-lg font-semibold text-center lg:text-left">
-              Follow Us
+              Contact Us
             </h2>
             <InteractiveSectionGroup
               rememberZIndex={false}
-              defaultScaleLevel="small"
+              defaultScaleLevel="medium"
             >
               <div className="flex space-x-6 mt-3">
-                <InteractiveSection
-                  sectionId="facebook"
-                  elementType="image"
-                  className="inline-block"
-                >
-                  <a href={facebookUrl} target="_blank" rel="noreferrer">
-                    <FontAwesomeIcon
-                      className="text-ec_text_darkmode"
-                      size="3x"
-                      icon={faFacebookSquare}
-                    />
-                  </a>
-                </InteractiveSection>
                 <InteractiveSection
                   sectionId="instagram"
                   elementType="image"
@@ -110,6 +102,32 @@ const Footer = ({
                       className="text-ec_text_darkmode"
                       size="3x"
                       icon={faLinkedin}
+                    />
+                  </a>
+                </InteractiveSection>
+                <InteractiveSection
+                  sectionId="facebook"
+                  elementType="image"
+                  className="inline-block"
+                >
+                  <a href={facebookUrl} target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon
+                      className="text-ec_text_darkmode"
+                      size="3x"
+                      icon={faFacebookSquare}
+                    />
+                  </a>
+                </InteractiveSection>
+                <InteractiveSection
+                  sectionId="email"
+                  elementType="image"
+                  className="inline-block"
+                >
+                  <a href="mailto:info@eclubpolimi.it" target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon
+                      className="text-ec_text_darkmode"
+                      size="3x"
+                      icon={faEnvelope}
                     />
                   </a>
                 </InteractiveSection>
